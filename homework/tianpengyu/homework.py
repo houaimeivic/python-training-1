251ecb8772fbc112789d9e22c8b97c9e9353a1dd
HelloRain2017 <tianpy5210@gmail.com>
2017-08-17 10:54:07 +0800

    commit homework

--- homework/tianpengyu/homework.py
+++ homework/tianpengyu/homework.py
@@ -1,9 +1,8 @@
+# coding=utf-8
 import requests
 import json
 
-
 users = []
-user = {}
 
 def get_posts():
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
@@ -13,23 +12,23 @@ def get_users():
     response = requests.get('https://jsonplaceholder.typicode.com/users')
     return json.loads(response.content)
 
-def find_by_id(id,title,get_user):
-    for msg in get_user:
-        if(msg['id']==id):
-            user['title'] = title
-            user['name'] = msg['name']
-            users.append(user)
-
+def find_by_id(id,get_user):
+    for user in get_user:
+        if(user['id']==id):
+            return user['name']
 
 def main():
     global users
     posts = get_posts()
     get_user = get_users()
     for user in posts:
-        find_by_id(user['id'],user['title'],get_user)
+        account = {}
+        name = find_by_id(user['userId'],get_user)
+        account['title'] = user['title']
+        account['name'] = name
+        users.append(account)
     file = open('data.txt','w')
-    file.writelines(['{}\n'.format(line) for line in users])
-
+    file.writelines(['标题:{title},作者:{name}\n'.format(title=line['title'],name=line['name']) for line in users])
 
 if __name__ == '__main__':
     main()

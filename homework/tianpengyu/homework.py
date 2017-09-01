a2c354ffba83fa9865c4728822e94f058f0c1897
HelloRain2017 <tianpy5210@gmail.com>
2017-09-01 14:15:37 +0800

    1.恢复tutorial下的代码 2.文件尾加上空行 3.逗号后加上空格 4.if 后不加括号 5.修改函数 find_by_id为find_username_by_id

--- homework/tianpengyu/homework.py
+++ homework/tianpengyu/homework.py
@@ -12,9 +12,9 @@ def get_users():
     response = requests.get('https://jsonplaceholder.typicode.com/users')
     return json.loads(response.content)
 
-def find_by_id(id,get_user):
+def find_username_by_id(id,get_user):
     for user in get_user:
-        if(user['id']==id):
+        if user['id']==id:
             return user['name']
 
 def main():
@@ -23,12 +23,12 @@ def main():
     get_user = get_users()
     for user in posts:
         account = {}
-        name = find_by_id(user['userId'],get_user)
+        name = find_username_by_id(user['userId'],get_user)
         account['title'] = user['title']
         account['name'] = name
         users.append(account)
-    file = open('data.txt','w')
-    file.writelines(['标题:{title},作者:{name}\n'.format(title=line['title'],name=line['name']) for line in users])
+    file = open('data.txt', 'w')
+    file.writelines(['标题:{title}, 作者:{name}\n'.format(title=line['title'], name=line['name']) for line in users])
 
 if __name__ == '__main__':
     main()
